f0f71f19aeedf3b5bb068d131c3481ddffd9b2b3
AsMa Zh <zh.asmazahani@gmail.com>
2026-04-28 19:50:34 +0100

    Initial project setup with Spring Boot, Thymeleaf and JPA

---
+++ src/main/java/com/reservia/ReserviaApplication.java
@@ -0,0 +1,13 @@
+package com.reservia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserviaApplication.class, args);
+    }
+
+}
