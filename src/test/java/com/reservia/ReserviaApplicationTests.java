da0fc8b2bff860a889d2050315676169b6eab93d
AsMa Zh <zh.asmazahani@gmail.com>
2026-04-28 19:50:34 +0100

    Initial project setup with Spring Boot, Thymeleaf and JPA

---
+++ src/test/java/com/reservia/ReserviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserviaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
