666da67476f3739a2867e6343e4b9fb96a525203
AsMa Zh <zh.asmazahani@gmail.com>
2026-04-30 11:37:22 +0100

    updating path and security

--- src/main/java/com/reservia/config/SecurityConfig.java
+++ src/main/java/com/reservia/config/SecurityConfig.java
@@ -20,7 +20,8 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/auth/**").permitAll()
+                    .requestMatchers("/reservia/**").permitAll()
+                    .requestMatchers("/css/**" , "/fonts/**" , "/images/**", "/js/**").permitAll()
                     .requestMatchers("/admin/**").hasRole("ADMIN")
                     .anyRequest().authenticated()
             )
