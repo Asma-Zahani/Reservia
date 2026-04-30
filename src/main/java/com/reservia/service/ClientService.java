095e75b321b15a9fac31b1613fd21aa0c1e879d9
AsMa Zh <zh.asmazahani@gmail.com>
2026-04-30 13:38:16 +0100

    auth

--- src/main/java/com/reservia/service/ClientService.java
+++ src/main/java/com/reservia/service/ClientService.java
@@ -2,6 +2,7 @@ package com.reservia.service;
 
 import com.reservia.entity.Client;
 import com.reservia.repository.ClientRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,16 +11,16 @@ import java.util.Optional;
 public class ClientService {
 
     private final ClientRepository clientRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository,
+                         PasswordEncoder passwordEncoder) {
         this.clientRepository = clientRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Client register(Client client) {
+        client.setPassword(passwordEncoder.encode(client.getPassword()));
         return clientRepository.save(client);
-    }
-
-    public Optional<Client> login(String email) {
-        return clientRepository.findByEmail(email);
     }
 }
